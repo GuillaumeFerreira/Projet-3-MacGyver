2db372c2dc6fabfe696fe917f1033d91e4d94c5b
ferreira <guillaumeferreira00@gmail.com>
2017-11-04 16:36:13 +0100

    Ajout interaction Gardien

--- main.py
+++ main.py
@@ -173,6 +173,7 @@ class MacGayver(ElementLabyrinthe):
 
             self.objetRamasser=0
             self.nombreDeFoisDansZone=0
+            self.etreResortiDeLaZone=True
             #retourne True ou false pour savoir si MacGayver est dans la zone
         
         def macDansZoneGardien(self):
@@ -264,7 +265,15 @@ class Mur(ElementLabyrinthe):
             canvas.create_image(borderwidth+(decalageImgX), borderheight+(decalageImgY), anchor=NW, image=photo)
             canvas.place(x=self.positionX, y=self.positionY)
                 
+class Gardien(ElementLabyrinthe):
 
+        def __init__(self,FrameLabyrinthe,photo):
+            self.positionX=592
+            self.positionY=570
+            self.memoireNbObjet=0
+            canvasGardien= Canvas(FrameLabyrinthe,width=32, height=36, borderwidth=0,highlightthickness=0)
+            canvasGardien.create_image(0, 0, anchor=NW, image=photo)
+            canvasGardien.place(x=self.positionX, y=self.positionY)
 ################################################################################################## 
 ##################################################################################################
 
@@ -346,12 +355,59 @@ def main():
     FenGardien.place(x=50, y=400)
     Label(FenGardien, textvariable=stringVarGardien).pack()
     stringVarGardien.set("Personne ne sortira tant que je ne serai pas satisfait!")
-
     
-    ###############################################################################
+    #################################################
     #Initialisation et construction de labyrinthe
-    #Chargement de la variable de l image photo
     photo = PhotoImage(file="images/wall.gif")
+    creationDuLabyrinthe(photo)
+    #################################################
+    #################################################
+    #création de MacGayver
+    photoMacGayver=PhotoImage(file="images/macgyver.gif")
+    global Mac
+    Mac=MacGayver(Frame1,photoMacGayver)
+    #################################################
+    #################################################
+    #Creation de la liste d objets
+    photoObjet=PhotoImage(file="images/tc-image005.gif")
+    ListeObjet=[]
+    for i in range(6):
+        newObjet=Objet(listeCoord,Frame1,photoObjet,i)
+        ListeObjet.append(newObjet)
+    #################################################  
+    #################################################
+    #Creation du gardien   
+    photoGardien = PhotoImage(file="images/gardien.gif")
+    global GardienLab
+    GardienLab=Gardien(Frame1,photoGardien)
+    ##################################################
+    ##################################################
+    #petit escalier
+    photo4=Image.open('images/tc-image007.png')
+    canvas15 = Canvas(Frame1,width=32, height=36, borderwidth=0,highlightthickness=0)
+
+    resolution = (100,263)
+    img = ImageTk.PhotoImage(photo4.resize(resolution))
+    canvas15.create_image(-2, -200, anchor=NW, image=img)
+    canvas15.place(x=592, y=610)
+    ###################################################
+
+
+
+    #Si pression sur la touche directionnelle fleche droite du clavier déclenche fonction versDroite
+    fenetre.bind('<Right>',versDroite)
+    #Si pression sur la touche directionnelle fleche gauche du clavier déclenche fonction versGauche
+    fenetre.bind('<Left>', versGauche)
+    #Si pression sur la touche directionnelle fleche haut du clavier déclenche fonction versHaut
+    fenetre.bind('<Up>', versHaut)
+    #Si pression sur la touche directionnelle fleche bas du clavier déclenche fonction versBas
+    fenetre.bind('<Down>', versBas)
+
+    fenetre.mainloop()
+              
+def creationDuLabyrinthe(photo):
+    ###############################################################################
+    #Initialisation et construction de labyrinthe  
     #ouverture du fichier qui decrit 
     f = open("Mac.txt", "r")
     i=0
@@ -386,55 +442,7 @@ def main():
         i=i+1
     f.close()
     ################################################################################
-
-    #################################################
-    #création de MacGayver
-    photoMacGayver=PhotoImage(file="images/macgyver.gif")
-    global Mac
-    Mac=MacGayver(Frame1,photoMacGayver)
-    #################################################
-    #################################################
-    #Creation de la liste d objets
-    photoObjet=PhotoImage(file="images/tc-image005.gif")
-    ListeObjet=[]
-    for i in range(6):
-        newObjet=Objet(listeCoord,Frame1,photoObjet,i)
-        ListeObjet.append(newObjet)
-    #################################################  
-    #################################################
-    #Creation du gardien   
-    photo3 = PhotoImage(file="images/gardien.gif")
-
-    canvas14 = Canvas(Frame1,width=32, height=36, borderwidth=0,highlightthickness=0)
-    canvas14.create_image(0, 0, anchor=NW, image=photo3)
-    canvas14.place(x=592, y=570)
-    ##################################################
-    ##################################################
-    #petit escalier
-    photo4=Image.open('images/tc-image007.png')
-    canvas15 = Canvas(Frame1,width=32, height=36, borderwidth=0,highlightthickness=0)
-
-    resolution = (100,263)
-    img = ImageTk.PhotoImage(photo4.resize(resolution))
-    canvas15.create_image(-2, -200, anchor=NW, image=img)
-    canvas15.place(x=592, y=610)
-    ###################################################
-
-
-
-    #Si pression sur la touche directionnelle fleche droite du clavier déclenche fonction versDroite
-    fenetre.bind('<Right>',versDroite)
-    #Si pression sur la touche directionnelle fleche gauche du clavier déclenche fonction versGauche
-    fenetre.bind('<Left>', versGauche)
-    #Si pression sur la touche directionnelle fleche haut du clavier déclenche fonction versHaut
-    fenetre.bind('<Up>', versHaut)
-    #Si pression sur la touche directionnelle fleche bas du clavier déclenche fonction versBas
-    fenetre.bind('<Down>', versBas)
-
     
-    fenetre.mainloop()
-              
-        
 def versDroite(evt):
    Mac.deplacementVersDroite()
    ramasserObjetEtVictoire()
@@ -455,9 +463,29 @@ def versBas(evt):
 def ramasserObjetEtVictoire():
    ramasserLesObjets()
    nombreObjetRamasser()
-   if Mac.macDansZoneGardien():
-        Mac.nombreDeFoisDansZone=+1
-        gagner(Mac.positionX,Mac.positionY)
+   if Mac.macDansZoneGardien() and Mac.etreResortiDeLaZone:
+        Mac.etreResortiDeLaZone=False
+        Mac.nombreDeFoisDansZone=Mac.nombreDeFoisDansZone+1
+        if Mac.objetRamasser==6:
+            gagner()
+        else:
+            if Mac.nombreDeFoisDansZone>=3:
+                perdu()
+            elif GardienLab.memoireNbObjet==Mac.objetRamasser and Mac.nombreDeFoisDansZone<3:
+                    #gardien en tres en colere
+                    stringVarGardien.set("Tu te crois malin!\n Tu n'as rien récupéré de plus, va t'en d'ici tout de suite!!!")
+            elif Mac.nombreDeFoisDansZone==2 and GardienLab.memoireNbObjet!=Mac.objetRamasser:
+                stringVarGardien.set("C'est la deuxième fois que tu viens me voir!\n Tu n'as que " + str(Mac.objetRamasser) +" objets!\n Je te déconseille de revenir me voir une troisième fois \n sans tous les objets!")
+            else:
+                    #gardien en colere
+                if Mac.objetRamasser==1:
+                    stringVarGardien.set("Quoi ? Seulement " + str(Mac.objetRamasser) +" objet ramassé!\n Tu oses venir me voir sans avoir fait le job.\n Ne reviens me voir que si tu les as tous retrouvés!")
+                else:
+                    stringVarGardien.set("Quoi ? Seulement " + str(Mac.objetRamasser) +" objets ramassés!\n Tu oses venir me voir sans avoir fait le job.\n Ne reviens me voir que si tu les as tous retrouvés!")
+                
+        GardienLab.memoireNbObjet=Mac.objetRamasser
+   else:
+        Mac.etreResortiDeLaZone=True
         
 def ramasserLesObjets():
         #On regarde si MacGayver se trouve sur un objet si oui , on le supprime
@@ -470,22 +498,29 @@ def nombreObjetRamasser():
     if Mac.objetRamasser==0:
         stringVar.set(" - ")
     elif Mac.objetRamasser==1:
-        stringVar.set( str(Mac.objetRamasser) +" objet ramassé.")
+        stringVar.set(str(Mac.objetRamasser) +" objet ramassé.")
     else:
         stringVar.set(str(Mac.objetRamasser) +" objets ramassés.")
         
 
 
 
-    
-RevenuSortiZone=True
-def gagner(x,y):
-    global nbPresentation
-    global nbobj
-    global RevenuSortiZone
-    
-    if x>555 and y>500 and Mac.objetRamasser==6:
-        global fenetreGagner
+def perdu():
+        global fenetrePerdu
+        fenetrePerdu = Tk()
+        fenetrePerdu.title('Game Over')
+        fenetrePerdu.configure(width=400,height=250)
+        w = Label(fenetrePerdu, text="Gardien : Je t'avais prévenu ! Bye bye mon ami!\n \n Tu as perdu, mais ne te décourage pas, recommence.")
+        w.place(x=50, y=40)
+        fenetre.unbind('<Right>') 
+        fenetre.unbind('<Left>')
+        fenetre.unbind('<Up>')
+        fenetre.unbind('<Down>')
+        Button(fenetrePerdu,text='Quitter', command=quit).place(x=90, y=180)
+        Button(fenetrePerdu,text='Rejouer' , command=rejouerLoose).place(x=230, y=180)
+
+def gagner():
+        
         fenetreGagner = Tk()
         fenetreGagner.title('Victoire')
         fenetreGagner.configure(width=400,height=150)
@@ -498,41 +533,7 @@ def gagner(x,y):
         Button(fenetreGagner,text='Quitter', command=quit).place(x=120, y=80)
         Button(fenetreGagner,text='Rejouer' , command=rejouer).place(x=250, y=80)
         
-    elif x>555 and y>500 and Mac.objetRamasser<6:
-        
-        if nbobj!=Mac.objetRamasser:
-            nbobj=Mac.objetRamasser
-            nbPresentation=nbPresentation+1
-            RevenuSortiZone=False
-        elif nbobj==Mac.objetRamasser and RevenuSortiZone:
-            stringVarGardien.set("Tu te crois malin!\n Tu n'as rien récupéré de plus, va t'en d'ici tout de suite!!!")
-            nbPresentation=nbPresentation+1
-            RevenuSortiZone=False
-        print(nbPresentation)                        
-        if nbPresentation==1:
-            if Mac.objetRamasser==1:
-                stringVarGardien.set("Quoi ? Seulement " + str(Mac.objetRamasser) +" objet ramassé!\n Tu oses venir me voir sans avoir fait le job.\n Ne reviens me voir que si tu les as tous retrouvés!")
-            else:
-                stringVarGardien.set("Quoi ? Seulement " + str(lMac.objetRamasser) +" objets ramassés!\n Tu oses venir me voir sans avoir fait le job.\n Ne reviens me voir que si tu les as tous retrouvés!")
-        elif nbPresentation==2 and nbobj!=Mac.objetRamasser:
-            stringVarGardien.set("C'est la deuxième fois que tu viens me voir!\n Tu n'as que " + str(Mac.objetRamasser) +" objets!\n Je te déconseille de revenir me voir une troisième fois \n sans tous les objets!")
-        elif nbPresentation==3:
-            global fenetrePerdu
-            fenetrePerdu = Tk()
-            fenetrePerdu.title('Game Over')
-            fenetrePerdu.configure(width=400,height=250)
-            w = Label(fenetrePerdu, text="Gardien : Je t'avais prévenu ! Bye bye mon ami!\n \n Tu as perdu, mais ne te décourage pas, recommence.")
-            w.place(x=50, y=40)
-            fenetre.unbind('<Right>') 
-            fenetre.unbind('<Left>')
-            fenetre.unbind('<Up>')
-            fenetre.unbind('<Down>')
-            Button(fenetrePerdu,text='Quitter', command=quit).place(x=90, y=180)
-            Button(fenetrePerdu,text='Rejouer' , command=rejouerLoose).place(x=230, y=180)
-    else:
-        
-        RevenuSortiZone=True
-        print("Pas encore au bout du labyrinthe")
+
 
 def rejouer():
     fenetreGagner.destroy()
